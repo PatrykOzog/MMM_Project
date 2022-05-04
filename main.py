de04389d528ca591d6e7dc0369d36583d2d979ae
PatrykOzog <104310014+PatrykOzog@users.noreply.github.com>
2022-05-04 14:18:18 +0200

    All generators are working fine, need to name the labels and create text inputs for all variables

--- main.py
+++ main.py
@@ -5,49 +5,36 @@ from scipy import signal
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
-def sine_gen(t, V, f):
-    sin = V * np.sin(2 * np.pi * f * t)
-    plt.plot(time, sin)
-    plt.title('Sine wave')
-    plt.xlabel('Time')
-    plt.ylabel('Amplitude')
-    plt.ylim(-V - 1, V + 1)
-    plt.grid(True, which='both')
-    plt.axhline(y=0, color='k')
-    return plt.gcf()
-
-
-def square_gen(t, V, f):
-    square = V * signal.square(2 * np.pi * f * t)
-    plt.plot(time, square)
-    plt.title('Square wave')
-    plt.xlabel('Time')
-    plt.ylabel('Amplitude')
-    plt.ylim(-V - 1, V + 1)
-    plt.grid(True, which='both')
-    plt.axhline(y=0, color='k')
-    return plt.gcf()
-
-
-def triangle_gen(t, V, f, tri):
-    plt.plot(time, tri)
-    plt.title('Triangle wave')
-    plt.xlabel('Time')
-    plt.ylabel('Amplitude')
-    plt.ylim(-V - 1, V + 1)
-    plt.grid(True, which='both')
-    plt.axhline(y=0, color='k')
-    return plt.gcf()
-
-
-def triangle_response(tri, L, R, Km, KT, b, J, I, w, t):
-    for i in range(len(tri)):
-        if i + 1 == len(tri):
+def response(name, sig, L, R, Km, KT, b, J, t):
+    figure, axis = plt.subplots(3, figsize=(12,9))
+    I = [0]
+    w = [0]
+    for i in range(len(sig)):
+        if i + 1 == len(sig):
             break
         else:
             ts = t[i + 1] - t[i]
-            I.append(I[i] + ts * (tri[i] / L - (R * I[i]) / L - (Km * w[i]) / L))
+            I.append(I[i] + ts * (sig[i] / L - (R * I[i]) / L - (Km * w[i]) / L))
             w.append(w[i] + ts * ((KT * I[i]) / J - (b * w[i]) / J))
+
+    axis[0].plot(t, sig)
+    axis[1].plot(t, I)
+    axis[2].plot(t, w)
+
+    axis[0].set_title(f"{name} wave")
+    axis[0].set_ylabel("U[V]")
+    axis[0].set_xlabel("t[s]")
+    axis[1].set_title("Current")
+    axis[2].set_title("Angular velocity")
+    plt.subplots_adjust(hspace=1)
+
+    # plt.title('Triangle wave')
+    # plt.xlabel('Time')
+    # plt.ylabel('Amplitude')
+    # plt.ylim(-V - 1, V + 1)
+    # plt.grid(True, which='both')
+    # plt.axhline(y=0, color='k')
+    return plt.gcf()
 
 
 def draw_figure(canvas, figure):
@@ -88,7 +75,7 @@ first_column = [
 ]
 
 second_column = [
-    [sg.Canvas(size=(500, 500), key="-CANVAS-")],
+    [sg.Canvas(size=(800, 800), key="-CANVAS-")],
 ]
 
 layout = [
@@ -118,25 +105,28 @@ while True:
         except ValueError:
             sg.popup("An error has occurred\nValue in the text box is incorrect!\n", title="ERROR", grab_anywhere=True)
             isError = True
-            # window.refresh()
 
     if not isError:
         if event == "Generate triangle wave":
             fig_agg.get_tk_widget().forget()
             plt.close('all')
-            triangle = amplitude * signal.sawtooth(2 * np.pi * frequency * time, 0.5)
-            fig_agg = draw_figure(window["-CANVAS-"].TKCanvas, triangle_gen(time, amplitude, frequency, triangle))
-            triangle_response(triangle, inductance, resistance, km, kT, damping, Moment_inertia, current, omega, time)
+            triangle = amplitude * signal.sawtooth(2 * np.pi * frequency * time - 1.5*np.pi, 0.5)
+            wave_name = "Triangle"
+            fig_agg = draw_figure(window["-CANVAS-"].TKCanvas, response(wave_name, triangle, inductance, resistance, km, kT, damping, Moment_inertia, time))
             window.refresh()
         elif event == "Generate square wave":
             fig_agg.get_tk_widget().forget()
             plt.close('all')
-            fig_agg = draw_figure(window["-CANVAS-"].TKCanvas, square_gen(time, amplitude, frequency))
+            square = amplitude * signal.square(2 * np.pi * frequency * time)
+            wave_name = "Square"
+            fig_agg = draw_figure(window["-CANVAS-"].TKCanvas, response(wave_name, square, inductance, resistance, km, kT, damping, Moment_inertia, time))
             window.refresh()
         elif event == "Generate sine wave":
             fig_agg.get_tk_widget().forget()
             plt.close('all')
-            fig_agg = draw_figure(window["-CANVAS-"].TKCanvas, sine_gen(time, amplitude, frequency))
+            sin = amplitude * np.sin(2 * np.pi * frequency * time)
+            wave_name = "Sine"
+            fig_agg = draw_figure(window["-CANVAS-"].TKCanvas, response(wave_name, sin, inductance, resistance, km, kT, damping, Moment_inertia, time))
             window.refresh()
 
 window.close()
